97206708c716fd863e3234b77d7ba81d632140f3
sentiaix <knnxcs@gmail.com>
2025-06-02 09:43:03 +0900

    FeatL 11장 클리어

--- OJ/ch.11/ojtest11-3.c
+++ OJ/ch.11/ojtest11-3.c
@@ -1,85 +1,66 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-// <문제>: 복소수의 입력을 받고 합을 출력하여라. 구조체 사용 필수.
-// a+bi c+di >> (a+c)+(b+d)i
-// -1.0+2.4i -0.9-2.3i >> -0.1+0.1i
 
-// 문제 해결 과정
-// 1. 일단 main함수에서 문자를 char로 받고 float으로 변환을 테스트. >> 안됨
-// 1 - 1. 숫자를 정수부+소수부 + 허수부로 계산하기.
-// 2. main함수에서 로직을 완성시키기 (실수와 허수 분리 등)
-// 3. 구조체와 함수로 main함수에 있던 로직들을 모듈화하기
-
-struct complex_number{ // 실수 허수 저장
-    double real; // 실수 (real number)
-    double imag; // 허수 (imaginary number)
-};
-struct input_number{ // 숫자 저장
-    char number_1[20];
-    char number_2[20];
-};
-
-struct transfer{ // 위의 두 struct 묶기
-    struct complex_number CNum;
-    struct input_number In;
-};
+typedef struct complex{
+    double real;
+    double imag;
+} Complex;
 
 // function declaration
-struct input_number parsing();
-double str_to_double(char* str);
+double parse_number(const char *str, int *idx); // str에 표현된 숫자를 double로 변환함
+Complex parse_complex(const char *str);  // 실수부 허수부 분리
 
-int main(){
-    struct transfer trans;
+int main() {
+    char input[32];
+    Complex a, b, result;
 
-    scanf("%s %s", trans.In.number_1, trans.In.number_2);
-    
+    scanf("%s", input);
+    a = parse_complex(input);
+
+    scanf("%s", input);
+    b = parse_complex(input);
+
+    result.real = a.real + b.real;
+    result.imag = a.imag + b.imag;
+
+    printf("%.2f%+.2fi\n", result.real, result.imag);
     return 0;
 }
 
-struct input_number parsing(){
-    struct input_number n;
-    struct complex_number c;
-    char part_1[5]; // operator 앞 뒤로 나누어 입력을 처리함
-    char part_2[5]; // part1, part2와 같이 분리하여 검증하도록 하였음.
+// define function
+double parse_number(const char *str, int *idx){
+    double num = 0.0;
+    double frac = 0.0;
+    int sign = 1;
+    int i = *idx;
 
-    int i = 0;
-    while(1){
-        if(n.number_1[i]){
+    if (str[i] == '-'){sign = -1; i++;}
+    else if (str[i] == '+'){i++;}
 
-        }
-    }
-
-}
-
-double str_to_double(char* str){
-    double num = 0.0f;
-    double dot = 0.1; // 소수 몇번 나왔는지, n번째 소수면 더 작은 수로 처리해야함.
-    int sign = 1; // 양수 음수 저장
-    int i = 0;
-
-    // 부호 확인
-    if(str[i] == '-'){
-        sign = -1;
-        i++;
-    } else if(str[i] == '+'){
-        i++;
-    }
-    
-    // 정수부
     while(str[i] >= '0' && str[i] <= '9'){
-        // 0.0f에 10을 곱하면 첫 수행시 0.0f = 0.0f + str에 있는 값 
         num = num * 10 + (str[i] - '0');
+        i++;
     }
 
-    // 소수
-    if(str[i] == '.'){
+    if(str[i] == '.'){ // 소수부 연산
         i++;
+        double div = 10.0;
         while(str[i] >= '0' && str[i] <= '9'){
-            num = num + (str[i] - '0') * dot;
-            dot *= 0.1f;
+            frac += (str[i] - '0') / div;
+            div *= 10;
             i++;
         }
     }
 
-    return sign * num;
+    *idx = i;
+    return sign * (num + frac);
+}
+
+Complex parse_complex(const char *str){
+    Complex c;
+    int idx = 0;
+
+    c.real = parse_number(str, &idx);
+    c.imag = parse_number(str, &idx);  // 연산자부터 다시 읽음
+    return c;
 }
