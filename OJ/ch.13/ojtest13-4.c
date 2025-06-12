a4055fed09e55723a15edeeebc8364b640e534ec
sentiaix <knnxcs@gmail.com>
2025-06-12 09:37:27 +0900

    주석 한 줄 달았음

--- OJ/ch.13/ojtest13-4.c
+++ OJ/ch.13/ojtest13-4.c
@@ -30,13 +30,13 @@ char* to_str(int* str_size, unsigned int* arr){
         int temp_idx = 0; // num값과 함께 임시로 
 
         // 예외처리: 숫자 0은 그냥 '0'으로 출력
-        if (arr[i] == 0) { // 그저 입력이 0 인 경우
+        if(arr[i] == 0){ // 그저 입력이 0 인 경우
             temp[temp_idx++] = '0';
         }
         // 입력이 0 이 아닌 경우
-        else {
+        else{
             unsigned int num = arr[i]; // 배열의 값을 임시로 저장. 원래 값 손상 방지 + 코딩 중 쓰기 쉽게
-            while (num > 0) { // 계속 나누면 0.n이 돼서 자료형(int)에 의해 
+            while(num > 0){ // 계속 나누면 0.n이 돼서 자료형(int)에 의해 
                 // 뒷자릿수부터 저장하는거임!: 12345 > 54321로 저장됨
                 temp[temp_idx++] = (num % 10) + '0';
                 num /= 10;  // 숫자 1자리씩 나누기
@@ -49,7 +49,7 @@ char* to_str(int* str_size, unsigned int* arr){
         }
 
         // 마지막 숫자 뒤에 공백 추가 (마지막 케이스는 SPACE 없음)
-        if (i < 2) {
+        if(i < 2){
             result[idx++] = ' ';
         }
     }
