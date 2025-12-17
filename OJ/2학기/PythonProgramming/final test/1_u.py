ebede890090f2a6d5134be9bed8e8ffcd6e231da
sentiaix <knnxcs@gmail.com>
2025-12-17 18:59:14 +0900

    이 병신같은 새끼 이걸 왜 못했을까

--- OJ/2학기/PythonProgramming/final test/1_u.py
+++ OJ/2학기/PythonProgramming/final test/1_u.py
@@ -68,4 +68,10 @@ while(True):
         for k in range(i * 2 + 1):
             print("*", end='')
         print()
+    for i in range(n - 1):
+        for j in range(i + 1):
+            print(" ", end='')
+        for k in range(n*2 - i*2 - 3):
+            print("*", end='')
+        print()
     # 역출력 해야함
