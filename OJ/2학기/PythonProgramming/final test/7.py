00517ae17a237594037f4ee4a76a340713ca2bba
sentiaix <knnxcs@gmail.com>
2025-12-19 23:01:03 +0900

    7 cleared

--- OJ/2학기/PythonProgramming/final test/7.py
+++ OJ/2학기/PythonProgramming/final test/7.py
@@ -50,12 +50,94 @@ Type: Circle --> Area: 28.27
 Type: Circle --> Area: 50.27
 '''
 
+
+def capitalize_str(s):
+    s = s.capitalize()
+    if s == 'Rectangle':
+        return f"Rectangle"
+    elif s == 'Circle':
+        return f"Circle"
+    
+
 class Shape:
     def __init__(self, shape):
         self.shape = shape
+
     def print_shape(self):
-        print(f"Type: {self.shape}", end='')
+        print(f"Type: {self.shape} --> ", end='')
+
 
 class Rectangle(Shape):
-    def __init__(self, n):
-        pass
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__("Rectangle") # "Rectangle"
+        # 좌상단 꼭짓점 좌표
+        self.x1 = x1
+        self.y1 = y1
+        # 우하단 꼭짓점 좌표
+        self.x2 = x2
+        self.y2 = y2
+
+    def calcArea(self):
+        return (self.x2 - self.x1) * (self.y1 - self.y2)
+
+
+class Circle(Shape):
+    def __init__(self, x, y, r):
+        super().__init__("Circle") # "Circle"
+        self.x = x
+        self.y = y
+        self.r = r
+
+    def calcArea(self):
+        pi = 3.141592
+        return pi * self.r * self.r
+
+
+# -- 메인 코드 -- #
+shapeList = [] # shape를 string으로 저장함.
+posList = [] # shapeList에 1대1대응하는 tuple값을 저장함.
+
+
+# -- 입력 -- #
+print("Enter 3 shapes (rectangle/circle).")
+
+for i in range(1):
+    shape = input("Enter shape type: ")
+    shape = capitalize_str(shape)
+
+    if shape == 'Rectangle':
+        shapeList.append(shape)
+        pos = tuple(map(int, input("Enter x1,y1,x2,y2: ").split(',')))
+
+        if len(pos) != 4 :
+            print("Invaild input. Try again")
+            pos = tuple(map(int, input("Enter x1,y1,x2,y2: ").split(',')))
+        posList.append(pos)
+
+    elif shape == 'Circle':
+        shapeList.append(shape)
+        pos = tuple(map(int, input("Enter x,y,r: ").split(',')))
+        
+        if len(pos) != 3 :
+            print("Invaild input. Try again")
+            pos = tuple(map(int, input("Enter x,y,r: ").split(',')))
+        posList.append(pos)
+
+    else :
+        print("Invaild type.")
+
+# -- 출력 -- #
+
+print("--- Final Report ---")
+
+for i in range( len(shapeList) ):
+    if shapeList[i] == 'Rectangle':
+        r = Rectangle(*posList[i])
+        r.print_shape()
+        print(r.calcArea())
+    elif shapeList[i] == 'Circle':
+        c = Circle(*posList[i])
+        c.print_shape()
+        print("%.2f" % c.calcArea())
+    else :
+        print("ERROR!")
